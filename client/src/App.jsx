8bbdd6b4b38d2e3440de943b6b01b7922071691e
dire_wolf <aman.singhal@coditas.com>
2024-07-16 00:15:04 +0530

    created the required components and imported them

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { logo } from "./assets";
-import { Home, createPost } from "./pages";
+import { Home, CreatePost } from "./pages";
 
 const App = () => {
   return (
@@ -19,7 +19,21 @@ const App = () => {
         <Link to="/">
           <img src={logo} alt="logo" className="w-28 object-contain" />
         </Link>
+
+        <Link
+          to="./create-post"
+          className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+        >
+          Create
+        </Link>
       </header>
+
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#384370] min-h-[calc(100vh - 73px)]">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/create-post" element={<CreatePost />}></Route>
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 };
